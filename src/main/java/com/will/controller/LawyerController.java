1a1ecddb3af5f82beba20f7a48cc0c2903f74f23
kislee <dldndwjs@naver.com>
2020-10-30 13:27:40 +0900

    갈아엎기전 마지막 체크포인트

--- src/main/java/com/will/controller/LawyerController.java
+++ src/main/java/com/will/controller/LawyerController.java
@@ -1,11 +1,15 @@
 package com.will.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.will.domain.LawyerDetail;
+import com.will.domain.entity.LawyerEntity;
+import com.will.domain.repository.LawyerRepository;
 import com.will.dto.LawyerDto;
-
 import com.will.service.LawyerService;
 
 import lombok.AllArgsConstructor;
@@ -18,6 +22,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class LawyerController {
 	  private LawyerService lawyerService;
+	  private LawyerRepository lawyerRepository;
 	
 	
 	    
@@ -35,16 +40,32 @@ public class LawyerController {
 
 	        return "redirect:/user/login";
 	    }
+	  //현재사용자 정보 가져오기
+	    @GetMapping("/lawyer/user/info")
+	    public String dispCuurentUserinfo(@AuthenticationPrincipal LawyerDetail lawyerDetail, Model model) {
+	    	String id = lawyerDetail.getUsername();
+	    	LawyerEntity lawyerEntity = lawyerRepository.findLawyerEntityById(id);
+	    	model.addAttribute("currentUser", lawyerEntity);
+	    	return "info/myinfo";
+	    	
+	    }
 	    
-	    // 로그인 페이지
-	    @GetMapping("/user/login1")
-	    public String dispLogin1() {
-	        return "logintool/login";
+	  //현재 사용자 정보변경 페이지
+	    @GetMapping("/lawyer/resignup/{no}")
+	    public String resignup(@AuthenticationPrincipal LawyerDetail lawyerDetail, Model model) {
+	        String id  = lawyerDetail.getUsername();
+	        LawyerEntity lawyerEntity = lawyerRepository.findLawyerEntityById(id);
+
+	        model.addAttribute("currentUser", lawyerEntity);
+	        return "info/myinfo2";
 	    }
-	  
-	    // 로그인 결과 페이지
-	    @GetMapping("/user/login/result1")
-	    public String dispLoginResult1() {
-	        return "logintool/loginSuccess2";
+	    
+	    //현재 사용자 정보변경 처리
+	    @PostMapping("/lawyer/resignup/update/{no}")
+	    public String update(LawyerDto lawyerDto) {
+	    	   lawyerService.savePost(lawyerDto);
+	    	
+	    	return "redirect:/user/info";
 	    }
+	    
 }
